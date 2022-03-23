73830583d523af0820a8038b8b52784dca6a2435
Andrei Hubarevich <andrgch@gmail.com>
2022-03-23 09:57:42 +0000

    Logging configuration

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+import logging
 
 from blog.models import Post
 from blog.forms import CommentForm
 
+logger = logging.getLogger(__name__)
+
+
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
+  logger.debug(f'Got %d posts.', len(posts))
   return render(request, 'blog/index.html', {'posts': posts})
 
 
@@ -21,6 +26,7 @@ def post_detail(request, slug):
         comment.content_object = post
         comment.creator = request.user
         comment.save()
+        logger.info(f'Created comment on post %d from user %s.', post.id, request.user)
         return redirect(request.path_info)
     else:
       comment_form = CommentForm()
