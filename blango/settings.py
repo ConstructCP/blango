24e9346ec539a9426337146565de48339ab7c0e2
Andrei Hubarevich <andrgch@gmail.com>
2022-03-27 09:33:13 +0000

    Registration added

--- blango/settings.py
+++ blango/settings.py
@@ -186,6 +186,9 @@ class Dev(Configuration):
 
   DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+  EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+  ACCOUNT_ACTIVATION_DAYS = 7
+
 
 class Prod(Dev):
   DEBUG = False
