1e424ce5c197703b2cf22bd58cb118ca0c7a9fac
Andrei Hubarevich <andrgch@gmail.com>
2022-04-01 11:54:46 +0000

    Tag and Post ViewSets

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path, include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views as drf_auth_views
+from rest_framework.routers import DefaultRouter
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
 
-from .views import PostList, PostDetail, UserDetail
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
 
 urlpatterns = [
-  path('posts/', PostList.as_view(), name='api_post_list'),
-  path('posts/<int:pk>', PostDetail.as_view(), name='api_post_detail'),
   path('users/<str:email>', UserDetail.as_view(), name='api_user_detail')
 ]
 
@@ -25,7 +24,12 @@ schema_view = get_schema_view(
   public=True,
 )
 
+router = DefaultRouter()
+router.register('tags', TagViewSet)
+router.register("posts", PostViewSet)
+
 urlpatterns += [
+  path('', include(router.urls)),
   path('auth/', include('rest_framework.urls')),
   path('token-auth/', drf_auth_views.obtain_auth_token),
   re_path(
